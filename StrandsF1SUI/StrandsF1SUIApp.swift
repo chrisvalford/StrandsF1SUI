e6ef7021b0796f301d03d861d3c769bb68a1880b
Christopher Alford <chrisvalford@me.com>
2023-08-04 09:10:46 +0200

    Initial Commit

---
+++ StrandsF1SUI/StrandsF1SUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  StrandsF1SUIApp.swift
+//  StrandsF1SUI
+//
+//  Created by Christopher Alford on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct StrandsF1SUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
