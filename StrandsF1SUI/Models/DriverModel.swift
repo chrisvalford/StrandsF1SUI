de9bd48c39e417b72a0675ef9546d838876a6f9c
Christopher Alford <chrisvalford@me.com>
2023-09-06 15:59:41 +0200

    Added cache model and caching to Driver fetch

--- StrandsF1SUI/Models/DriverModel.swift
+++ StrandsF1SUI/Models/DriverModel.swift
@@ -6,6 +6,16 @@
 //
 
 import SwiftUI
+import CoreData
+
+enum APIError: Error {
+    case invalidURL
+    case invalidResponse
+    case noData
+    case decoderError
+    case cacheEmpty
+    case cacheError
+}
 
 /**
  * Model for the `DriverList` view
@@ -17,26 +27,47 @@ class DriverModel: ObservableObject {
     @Published var selectedSort = SortByField.none
     @Published var searchText = ""
 
+    fileprivate var context: NSManagedObjectContext = {
+        PersistenceController.shared.container.viewContext
+    }()
+
     init() {
         Task {
-            await fetch()
+            do {
+                try await fetch()
+            } catch {
+                print("**** Failed to retrieve Driver data with error \(error)")
+            }
         }
     }
 
     /**
      * fetch and decode all driver data from ergast service
      */
-    private func fetch() async {
+    private func fetch() async throws {
+        var jsonUrl = URL(string: "")
         do {
             guard let url = URL(string: "https://ergast.com/api/f1/current/drivers.json") else {
-                return
+                throw APIError.invalidURL
             }
+            jsonUrl = url
             let (data, response) = try await URLSession.shared.data(from: url)
             guard let httpResponse = response as? HTTPURLResponse,
                   (200...299).contains(httpResponse.statusCode) else {
-                print("Invalid Response")
-                return
+                throw APIError.invalidResponse
             }
+            try decode(data: data)
+            try updateCache(url: url, data: data)
+        } catch {
+            guard let data = try readCache(url: jsonUrl!) else {
+                throw APIError.cacheEmpty
+            }
+            try decode(data: data)
+        }
+    }
+
+    private func decode(data: Data) throws {
+        do {
             let decoder = JSONDecoder()
             decoder.dateDecodingStrategy = .formatted(dateFormatter)
             let results = try decoder.decode(DriverDataRoot.self, from: data)
@@ -45,7 +76,49 @@ class DriverModel: ObservableObject {
                 self.seriesTitle = "\(results.mrData?.series ?? "") - \(results.mrData?.driverTable?.season ?? "") Season"
             }
         } catch {
-            print(error)
+            throw APIError.decoderError
+        }
+    }
+
+    func updateCache(url: URL, data: Data) throws {
+        let fetchRequest: NSFetchRequest<CacheItem>
+        fetchRequest = CacheItem.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "url == %@", url as CVarArg)
+        do {
+            let cacheItems = try context.fetch(fetchRequest)
+            if cacheItems.count > 0 {   // Update url, data values if they exist
+                let cacheItem = cacheItems.first!
+                cacheItem.created = Date()
+                cacheItem.data = data
+                try context.save()
+            } else {                    // Otherwise crate a new entry
+                let cacheItem = CacheItem(context: context)
+                cacheItem.url = url
+                cacheItem.created = Date()
+                cacheItem.data = data
+                try context.save()
+            }
+        } catch {
+            throw APIError.cacheError
+        }
+    }
+
+    func readCache(url: URL) throws -> Data? {
+        let fetchRequest: NSFetchRequest<CacheItem>
+        fetchRequest = CacheItem.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "url == %@", url as CVarArg)
+        do {
+            let cacheItems = try context.fetch(fetchRequest)
+            if cacheItems.count > 0 {
+                guard let first = cacheItems.first else {
+                    throw APIError.cacheError
+                }
+                return first.data
+            } else {
+                throw APIError.cacheEmpty
+            }
+        } catch {
+            throw APIError.cacheError
         }
     }
 
