615e71247cdf84d6dc999334fb09d39028dbee23
Christopher Alford <chrisvalford@me.com>
2024-01-03 11:18:48 +0100

    Rearranged files into device groups and added tvOS Icon

---
+++ StrandsF1SUI/Strands_tvOS/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  StrandsF1SUI
+//
+//  Created by Christopher Alford on 3/1/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
